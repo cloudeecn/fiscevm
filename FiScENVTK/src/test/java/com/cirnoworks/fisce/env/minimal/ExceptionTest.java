2c9f9876465b63a9ee4719abb132586bd38d61ff
cloudeecn <cloudeecn@69b14270-f174-8f5e-9eb4-a458d7a51377>
2010-09-03 22:18:11 +0000

    change version
    git-svn-id: https://fiscevm.googlecode.com/svn/trunk@16 69b14270-f174-8f5e-9eb4-a458d7a51377

--- FiScENVTK/src/test/java/com/cirnoworks/fisce/env/minimal/ExceptionTest.java
+++ FiScENVTK/src/test/java/com/cirnoworks/fisce/env/minimal/ExceptionTest.java
@@ -7,9 +7,6 @@ import com.cirnoworks.fisce.vm.FiScEVM;
 public class ExceptionTest extends TestCase {
 
 	public void testException() throws Exception {
-		if (true) {
-			return;
-		}
 		FiScEVM context = TestInitializer.getContext();
 		context.bootFromClass("com/cirnoworks/fisce/test/ExceptionTester");
 		context.start();
