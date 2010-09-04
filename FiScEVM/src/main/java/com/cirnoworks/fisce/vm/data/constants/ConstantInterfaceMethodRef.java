30f8ca9fc87436ec2151f8ca8d1fc41f798c0b33
cloudeecn <cloudeecn@69b14270-f174-8f5e-9eb4-a458d7a51377>
2010-09-04 20:25:55 +0000

    copyright terms
    git-svn-id: https://fiscevm.googlecode.com/svn/trunk@18 69b14270-f174-8f5e-9eb4-a458d7a51377

--- FiScEVM/src/main/java/com/cirnoworks/fisce/vm/data/constants/ConstantInterfaceMethodRef.java
+++ FiScEVM/src/main/java/com/cirnoworks/fisce/vm/data/constants/ConstantInterfaceMethodRef.java
@@ -1,8 +1,8 @@
 /**
- *  Copyright 2010 Yuxuan Huang
+ *  Copyright 2010 Yuxuan Huang. All rights reserved.
  *  
  *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the Lesser GNU General Public License as published by
+ *  it under the terms of the GNU Lesser General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
