51e410f8ee414fdb3937dfe83285ec850d2981e1
cloudeecn@gmail.com <cloudeecn@gmail.com@b65c3487-50fd-737e-f2dc-7858143ae140>
2011-12-21 08:53:02 +0000

    script
    git-svn-id: https://libfisce.googlecode.com/svn/trunk@146 b65c3487-50fd-737e-f2dc-7858143ae140

--- shelljni/src/VMContext.c
+++ shelljni/src/VMContext.c
@@ -1,5 +1,6 @@
 #include "com_cirnoworks_libfisce_shell_VMContext.h"
 #include "../inc/fisce.h"
+#include "../inc/fisceprt.h"
 #include "../inc/fiscestu.h"
 #include "../inc/fiscedev.h"
 /*
@@ -17,11 +18,23 @@ JNIEXPORT jobject JNICALL Java_com_cirnoworks_libfisce_shell_VMContext_execute0(
  * Method:    initContext
  * Signature: ()V
  */
-JNIEXPORT void JNICALL Java_com_cirnoworks_libfisce_shell_VMContext_initContext(
+JNIEXPORT void JNICALL Java_com_cirnoworks_libfisce_shell_VMContext_initContext0(
 		JNIEnv *env, jobject self, jobject buf) {
-	fy_context *context=env->GetDirectBufferAddress(env,buf);
-	memchr(context,0,sizeof(fy_context));
+	fy_exception ex;
+	fy_exception *exception = &ex;
+	fy_context *context = (*env)->GetDirectBufferAddress(env, buf);
+	jclass runtimeException = (*env)->FindClass(env,
+			"java/lang/RuntimeException");
+	char msg[256];
 
+	ex.exceptionType = exception_none;
+	memset(context, 0, sizeof(fy_context));
+	fisceInitContext(context, exception);
+	if (exception->exceptionType != exception_none) {
+		sprintf_s(msg, sizeof(msg), "Fisce exception %s %s",
+				exception->exceptionName, exception->exceptionDesc);
+		(*env)->ThrowNew(env, runtimeException, msg);
+	}
 }
 
 /*
