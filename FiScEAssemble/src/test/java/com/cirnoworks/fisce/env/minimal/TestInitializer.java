b3c71e7fac05dad24e3ce2caa2b8f1baf2132c51
cloudeecn@gmail.com <cloudeecn@gmail.com@69b14270-f174-8f5e-9eb4-a458d7a51377>
2011-12-31 07:29:52 +0000

    merge fisce java to fisce c
    git-svn-id: https://fiscevm.googlecode.com/svn/trunk@214 69b14270-f174-8f5e-9eb4-a458d7a51377

--- FiScEAssemble/src/test/java/com/cirnoworks/fisce/env/minimal/TestInitializer.java
+++ FiScEAssemble/src/test/java/com/cirnoworks/fisce/env/minimal/TestInitializer.java
@@ -1,5 +1,5 @@
 /**
- *  Copyright 2010 Yuxuan Huang. All rights reserved.
+ *  Copyright 2010-2011 Yuxuan Huang. All rights reserved.
  *  
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License as published by
@@ -16,7 +16,17 @@
  */
 package com.cirnoworks.fisce.env.minimal;
 
+import java.io.InputStream;
+
+import org.w3c.dom.Element;
+
+import com.cirnoworks.fisce.intf.FiScEVM;
+import com.cirnoworks.fisce.intf.IThread;
+import com.cirnoworks.fisce.intf.IToolkit;
+import com.cirnoworks.fisce.intf.NativeHandlerTemplate;
 import com.cirnoworks.fisce.intf.NullConsole;
+import com.cirnoworks.fisce.intf.VMCriticalException;
+import com.cirnoworks.fisce.intf.VMException;
 import com.cirnoworks.fisce.vm.VMContext;
 import com.cirnoworks.fisce.vm.default_impl.ArrayHeap;
 import com.cirnoworks.fisce.vm.default_impl.ArrayThreadManager;
@@ -27,36 +37,80 @@ import com.cirnoworks.fisce.vm.default_impl.FastThreadManager;
 
 public class TestInitializer {
 
+	private static class TestToolkit implements IToolkit {
+
+		private class FailSupport extends NativeHandlerTemplate {
+
+			public void dealNative(int[] args, IThread thread)
+					throws VMException, VMCriticalException {
+				String str = args[0] == 0 ? "null" : context.getHeap()
+						.getString(args[0]);
+				throw new VMCriticalException("Test failed: " + str);
+			}
+
+			public String getUniqueName() {
+				return "EXCLUDE/fisce/test/TestService.fail.(Ljava/lang/String;)V";
+			}
+
+		}
+
+		private VMContext context;
+
+		public void setContext(FiScEVM context) {
+			this.context = (VMContext) context;
+		}
+
+		public void setupContext() {
+			context.registerNativeHandler(new FailSupport());
+		}
+
+		public InputStream getResourceByClassName(String className) {
+			return null;
+		}
+
+		public void saveData(Element data) throws VMCriticalException {
+
+		}
+
+		public void loadData(Element data) throws VMCriticalException {
+
+		}
+
+	}
+
 	public static VMContext getContext() {
 		VMContext vm = new VMContext();
 		vm.setClassLoader(new DefaultClassLoader());
 		vm.setHeap(new DefaultHeap());
 		vm.setThreadManager(new DefaultThreadManager());
 		vm.setConsole(new NullConsole());
-		//vm.setConsole(Log4JConsole.getConsole());
+		// vm.setConsole(Log4JConsole.getConsole());
 		vm.addToolkit(new BaseToolkit());
+		vm.addToolkit(new TestToolkit());
 		return vm;
 	}
-	
+
 	public static VMContext getFastContext() {
 		VMContext vm = new VMContext();
 		vm.setClassLoader(new DefaultClassLoader());
 		vm.setHeap(new DefaultHeap());
 		vm.setThreadManager(new FastThreadManager());
 		vm.setConsole(new NullConsole());
-		//vm.setConsole(Log4JConsole.getConsole());
+		// vm.setConsole(Log4JConsole.getConsole());
 		vm.addToolkit(new BaseToolkit());
+		vm.addToolkit(new TestToolkit());
 		return vm;
 	}
-	
+
 	public static VMContext getArrayContext() {
 		VMContext vm = new VMContext();
 		vm.setClassLoader(new DefaultClassLoader());
 		vm.setHeap(new ArrayHeap());
 		vm.setThreadManager(new ArrayThreadManager());
 		vm.setConsole(new NullConsole());
-		//vm.setConsole(Log4JConsole.getConsole());
+		// vm.setConsole(Log4JConsole.getConsole());
 		vm.addToolkit(new BaseToolkit());
+		vm.addToolkit(new TestToolkit());
 		return vm;
 	}
 }
