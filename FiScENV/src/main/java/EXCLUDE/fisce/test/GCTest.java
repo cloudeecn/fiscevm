fd7a403a02a089b8ce728f80c264b1f233a3e7b2
cloudeecn@gmail.com <cloudeecn@gmail.com@69b14270-f174-8f5e-9eb4-a458d7a51377>
2012-03-29 16:07:34 +0000

    change to 1.6
    git-svn-id: https://fiscevm.googlecode.com/svn/branches/201203.better.reflection@247 69b14270-f174-8f5e-9eb4-a458d7a51377

--- FiScENV/src/main/java/EXCLUDE/fisce/test/GCTest.java
+++ FiScENV/src/main/java/EXCLUDE/fisce/test/GCTest.java
@@ -3,15 +3,19 @@ package EXCLUDE.fisce.test;
 public class GCTest extends TestService {
 	public static void main(String[] args) {
 		try {
-			for (int i = 0; i < 5; i++) {
+			for (int i = 0; i < 10; i++) {
 				System.out.println(i + "+");
-				for (int j = 0; j < 1000; j++) {
+				for (int j = 0; j < 100; j++) {
 					Thread.sleep(1);
 					// byte[] b = new byte[10000];
 				}
 				System.gc();
 				System.out.println(i + ".");
 			}
+			System.gc();
+			System.gc();
+			System.gc();
+			System.gc();
 		} catch (Exception e) {
 			fail(e.toString());
 		}
