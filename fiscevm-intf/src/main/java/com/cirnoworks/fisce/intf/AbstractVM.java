c41608b0a74d6f0ef3228997230c2680774158d9
cloudee <cloudeecn@gmail.com>
2013-09-12 02:23:55 +0800

    refactor assembly logic

--- fiscevm-intf/src/main/java/com/cirnoworks/fisce/intf/AbstractVM.java
+++ fiscevm-intf/src/main/java/com/cirnoworks/fisce/intf/AbstractVM.java
@@ -4,6 +4,9 @@ import java.io.InputStream;
 
 public abstract class AbstractVM implements FiScEVM {
 	public InputStream getInputStream(String name) {
+		if (name.startsWith("/")) {
+			name = name.substring(1);
+		}
 		InputStream is = null;
 		for (IToolkit toolkit : this.getToolkits()) {
 			is = toolkit.getResourceByName(name);
