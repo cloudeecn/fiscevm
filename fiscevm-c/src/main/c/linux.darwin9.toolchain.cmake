c302b2ac6c94eac78b57c2ee7509b61d48e2a41b
Cloudee <cloudeecn@gmail.com>
2013-09-18 00:13:34 +0800

    try cross compile for osx in linux

---
+++ fiscevm-c/src/main/c/linux.darwin9.toolchain.cmake
@@ -0,0 +1,23 @@
+# the name of the target operating system
+SET(CMAKE_SYSTEM_NAME Apple)
+SET(APPLE on)
+
+# Choose an appropriate compiler prefix
+option(COMPILER_PREFIX "Prefix of the compiler default: x86_64-apple-darwin9" "x86_64-apple-darwin9")
+
+# which compilers to use for C and C++
+find_program(CMAKE_C_COMPILER NAMES ${COMPILER_PREFIX}-gcc)
+#SET(CMAKE_C_COMPILER ${COMPILER_PREFIX}-gcc)
+find_program(CMAKE_CXX_COMPILER NAMES ${COMPILER_PREFIX}-g++)
+#SET(CMAKE_CXX_COMPILER ${COMPILER_PREFIX}-g++)
+
+
+# here is the target environment located
+SET(CMAKE_FIND_ROOT_PATH  /usr/${COMPILER_PREFIX})
+
+# adjust the default behaviour of the FIND_XXX() commands:
+# search headers and libraries in the target environment, search 
+# programs in the host environment
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
