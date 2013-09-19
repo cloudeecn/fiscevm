9fa2cbffacd7d08ee6190b5539dba24962c7eabd
Cloudee <cloudeecn@gmail.com>
2013-09-19 22:48:23 +0800

    license and build policy

--- fiscevm-c/src/main/c/fyc/Data.h
+++ fiscevm-c/src/main/c/fyc/Data.h
@@ -1,20 +1,20 @@
 /**
  *  Copyright 2010-2013 Yuxuan Huang. All rights reserved.
  *
- * This file is part of libfisce.
+ * This file is part offiscevm
  *
- * libfisce is free software: you can redistribute it and/or modify
+ *fiscevmis free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * any later version.
  *
- * libfisce is distributed in the hope that it will be useful,
+ *fiscevmis distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with libfisce.  If not, see <http://www.gnu.org/licenses/>.
+ * along withfiscevm  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef FY_DATA_H
