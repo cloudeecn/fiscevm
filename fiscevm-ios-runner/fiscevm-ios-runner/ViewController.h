8ae3db13ff0a378e661ffdef1a55a244fe22b9a6
Cloudee <cloudeecn@gmail.com>
2015-01-25 11:26:42 +0800

    ios-runner

---
+++ fiscevm-ios-runner/fiscevm-ios-runner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  fiscevm-ios-runner
+//
+//  Created by Cloudee on 1/20/15.
+//  Copyright (c) 2015 Cirnoworks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ fiscevm-ios-runner/fiscevm-ios-runner/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  fiscevm-ios-runner
+//
+//  Created by Cloudee on 1/20/15.
+//  Copyright (c) 2015 Cirnoworks. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
