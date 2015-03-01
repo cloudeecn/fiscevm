9dc93cbbb17dbdce8653cc58333abba0fee9040b
Cloudee <cloudeecn@gmail.com>
2015-03-01 16:47:43 +0800

    vmgen script

--- fiscevm-c-native/src/vmgen.sh
+++ fiscevm-c-native/src/vmgen.sh
@@ -2,4 +2,4 @@
 ABSPATH=$(cd "$(dirname "$0")"; pwd)
 cd $ABSPATH/main/vmcore
 vmgen fisce.vmg
-sed -i -e 's/(Cell)//g; s/^#line.*//g' fisce-vm.i
+sed -i ".vmbak" -e 's/(Cell)//g; s/^#line.*//g' fisce-vm.i
